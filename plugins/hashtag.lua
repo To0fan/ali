bf58dd64e6d25b117055d391f47b9f2e430d1b0a
To0fan <toofaneariyayi@gmail.com>
2016-12-03 23:47:54 +0330

    Add files via upload

--- plugins/hashtag.lua
+++ plugins/hashtag.lua
@@ -10,6 +10,8 @@ local function warn_user(user_id, chat_id, user_print, user_user)
     if hashonredis then
         if tonumber(warn_chat) ~= 0 then
             if tonumber(hashonredis) >= 2 then
+				--send_large_msg(channel, string.gsub('shoma baraye bar X  link ersal kardid va kick mishavid', 'X', tostring(hashonredis)) ,ok_cb, false)
+				--redis:set(hash, 0)
 				send_large_msg(channel, 'Name: '..string.gsub(user_print, "_", " ")..'\nID: @'..user_user..'\n❌شما برای بار دوم پیام حاوی لینک تبلیغ ارسال کردید و برطبق قوانین از این گروه اخراج میشوید❌' ,ok_cb, false)
 				kick_user(user_id, chat_id)
                 redis:getset(hash, 0)
@@ -66,7 +68,7 @@ local function run(msg, matches)
 		if msg.media then
 
 		if msg.media.caption then -- msg.media.caption
-			if not msg.media.caption:match("#س") and not msg.media.caption:match("#ج") and not msg.media.caption:match("#سوال") and not msg.media.caption:match("#جواب") and not msg.media.caption:match("#پرسش") and not msg.media.caption:match("#پاسخ") then
+			if not msg.media.caption:match("#پیشنهاد") and not msg.media.caption:match("#نکته") and not  msg.media.caption:match("#سوال") and not msg.media.caption:match("#جواب") and not msg.media.caption:match("#انتقاد") and not msg.media.caption:match("#دغدغه") and not msg.media.caption:match("#حقوقی") and not msg.media.caption:match("#هستم") and not msg.media.caption:match("#حمایت") then
 				delete_msg(msg.id, ok_cb, false)
 				if tgs == "yes" then
 					if msg.media.caption:lower():match("telegram.me/") or msg.media.caption:lower():match("tlgrm.me/") then
@@ -79,7 +81,7 @@ local function run(msg, matches)
 			else
 				return
 			end
-			if msg.media.caption:match("#س") or msg.media.caption:match("#ج") or  msg.media.caption:match("#سوال") or msg.media.caption:match("#جواب") or msg.media.caption:match("#پرسش") or msg.media.caption:match("#پاسخ") then
+			if msg.media.caption:match("#پیشنهاد") or msg.media.caption:match("#نکته") or  msg.media.caption:match("#سوال") or msg.media.caption:match("#جواب") or msg.media.caption:match("#انتقاد") or msg.media.caption:match("#دغدغه") or msg.media.caption:match("#حقوقی") or msg.media.caption:match("#هستم") or msg.media.caption:match("#حمایت") then
 				if msg.media.caption:lower():match("telegram.me/") or msg.media.caption:lower():match("tlgrm.me/") then
 					if tgs == "yes" then
 						delete_msg(msg.id, ok_cb, false)
@@ -92,7 +94,7 @@ local function run(msg, matches)
 		end
 		if msg.fwd_from then
 		if msg.fwd_from.title then -- msg.fwd
-			if not msg.fwd_from.title:match("#س") and not msg.fwd_from.title:match("#ج") and  not msg.fwd_from.title:match("#سوال") and not msg.fwd_from.title:match("#جواب") and not msg.fwd_from.title:match("#پرسش") and not msg.fwd_from.title:match("#پاسخ") then
+			if not msg.fwd_from.title:match("#پیشنهاد") and not msg.fwd_from.title:match("#نکته") and not  msg.fwd_from.title:match("#سوال") and not msg.fwd_from.title:match("#جواب") and not msg.fwd_from.title:match("#انتقاد") and not msg.fwd_from.title:match("#دغدغه") and not msg.fwd_from.title:match("#حقوقی") and not msg.fwd_from.title:match("#هستم") and not msg.fwd_from.title:match("#حمایت") then
 				delete_msg(msg.id, ok_cb, false)
 				if tgs == "yes" then
 					if msg.fwd_from.title:lower():match("telegram.me/") or msg.fwd_from.title:lower():match("tlgrm.me/") then
@@ -102,7 +104,7 @@ local function run(msg, matches)
 					end
 				end
 			end
-			if msg.fwd_from.title:match("#س") or msg.fwd_from.title:match("#ج") or  msg.fwd_from.title:match("#سوال") or msg.fwd_from.title:match("#جواب") or msg.fwd_from.title:match("#پرسش") or msg.fwd_from.title:match("#پاسخ") then
+			if msg.fwd_from.title:match("#پیشنهاد") or msg.fwd_from.title:match("#نکته") or  msg.fwd_from.title:match("#سوال") or msg.fwd_from.title:match("#جواب") or msg.fwd_from.title:match("#انتقاد") or msg.fwd_from.title:match("#دغدغه") or msg.fwd_from.title:match("#حقوقی") or msg.fwd_from.title:match("#هستم") or msg.fwd_from.title:match("#حمایت") then
 				if msg.fwd_from.title:lower():match("telegram.me/") or msg.fwd_from.title:lower():match("tlgrm.me/") then
 					if tgs == "yes" then
 						delete_msg(msg.id, ok_cb, false)
@@ -113,7 +115,7 @@ local function run(msg, matches)
 		end
 		end
 		if msg.text then -- msg.text
-			if not msg.text:match("#س") and not msg.text:match("#ج") and  not msg.text:match("#سوال") and not msg.text:match("#جواب") and not msg.text:match("#پرسش") and not msg.text:match("#پاسخ") then
+			if not msg.text:match("#پیشنهاد") and not msg.text:match("#نکته") and not  msg.text:match("#سوال") and not msg.text:match("#جواب") and not msg.text:match("#انتقاد") and not msg.text:match("#دغدغه") and not msg.text:match("#حقوقی") and not msg.text:match("#هستم") and not msg.text:match("#حمایت") then
 				delete_msg(msg.id, ok_cb, false)	
 				if tgs == "yes" then
 					if msg.text:lower():match("telegram.me/") or msg.text:lower():match("tlgrm.me/") then
@@ -123,7 +125,7 @@ local function run(msg, matches)
 					end
 				end
 			end
-			if msg.text:match("#س") or msg.text:match("#ج") or  msg.text:match("#سوال") or msg.text:match("#جواب") or msg.text:match("#پرسش") or msg.text:match("#پاسخ") then
+			if msg.text:match("#پیشنهاد") or msg.text:match("#نکته") or  msg.text:match("#سوال") or msg.text:match("#جواب") or msg.text:match("#انتقاد") or msg.text:match("#دغدغه") or msg.text:match("#حقوقی") or msg.text:match("#هستم") or msg.text:match("#حمایت") then
 				if msg.text:lower():match("telegram.me/") or msg.text:lower():match("tlgrm.me/") then
 					if tgs == "yes" then
 						delete_msg(msg.id, ok_cb, false)
@@ -145,4 +147,4 @@ return {
 
 
 end
---@To0fan
+
